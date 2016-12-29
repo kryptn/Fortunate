abe4ee00722afdfbfaba5c92a03bd4141a820aaa
kryptn <kryptn@gmail.com>
2016-12-28 21:49:30 -0700

    Added basework for private fortune keys

--- fortunes/app.py
+++ fortunes/app.py
@@ -19,7 +19,8 @@ class User(db.Model):
     def __init__(self, ip):
         self.ip = ip
 
-    def get_or_create(self, ip):
+    @staticmethod
+    def get_or_create(ip):
         user = User.query.filter_by(ip=ip).first()
         if user:
             return user
@@ -36,11 +37,12 @@ class Key(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     token = db.Column(db.String(16), unique=True)
+    private = db.Column(db.Boolean)
 
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     user = db.relationship('User', backref=db.backref('keys', lazy='joined'))
 
-    def __init__(self, user):
+    def __init__(self, user, private=False):
         self.user = user
         result = True
         while result:
@@ -48,6 +50,14 @@ class Key(db.Model):
             result = Key.query.filter_by(token=token).first()
 
         self.token = token
+        self.private = private
+
+    @staticmethod
+    def create(user, private=False):
+        key = Key(user, private)
+        db.session.add(key)
+        db.session.commit()
+        return key
 
 
 class Fortune(db.Model):
@@ -62,10 +72,18 @@ class Fortune(db.Model):
         self.text = text
         self.key = key
 
+    @staticmethod
+    def get_random(token=None, only_token=True):
+        pass
+
 
 class KeyView(Resource):
     def get(self):
-        pass
+        user = User.get_or_create(request.remote_addr)
+        private = request.form.get('private', False)
+        key = Key.create(user, private)
+        return {'token': key.token, 'private': private}
+
 
 class FortuneView(Resource):
     def get(self):
