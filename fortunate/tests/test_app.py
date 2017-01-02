19a314a4a4f9b0fd2c63d61a19be4061557264cd
kryptn <kryptn@gmail.com>
2017-01-02 10:20:28 -0700

    Added test for make_app

---
+++ fortunate/tests/test_app.py
@@ -0,0 +1,16 @@
+import unittest
+
+from fortunate import make_app
+
+class AppTest(unittest.TestCase):
+
+    def test_app(self):
+        app = make_app()
+        self.assertEqual(app.name, 'fortunate')
+
+    def test_additional_settings(self):
+        class TestSettings:
+            TESTCONFIG=True
+
+        app = make_app(TestSettings)
+        self.assertTrue(app.config['TESTCONFIG'])
