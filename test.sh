64fc2c009a0573a0d121a4a3285418151461b16d
kryptn <kryptn@gmail.com>
2017-01-02 08:54:42 -0700

    setup.py test added, and added test coverage?

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ tf='.venv_test'
 virtualenv $tf -p python
 $tf/bin/pip install -e fortunate
 export FORTUNATE_SETTINGS=test_settings.py
-$tf/bin/python -m fortunate.tests
+$tf/bin/python fortunate/setup.py test
 unset FORTUNATE_SETTINGS
 
 rm -rf $tf
