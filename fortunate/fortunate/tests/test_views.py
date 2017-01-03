d8d480968ed5207e8c5c9217795dd4580624ea5a
kryptn <kryptn@gmail.com>
2017-01-03 14:13:57 -0700

    Added more tests, gotta get dat coverage

--- fortunate/fortunate/tests/test_views.py
+++ fortunate/fortunate/tests/test_views.py
@@ -3,6 +3,13 @@ from fortunate import models, views
 from fortunate.tests.base import Base
 
 class ViewsTest(Base):
+    
+    fortunes = list('abcdefg')
+
+    def set_fortune(self, token, fortune):
+        payload = {'token': token, 'fortune': fortune}
+        response = self.client.post('/fortune/', data=payload)
+        return response                
 
     def test_token_get(self):
         response = self.client.get('/token/')
@@ -10,7 +17,25 @@ class ViewsTest(Base):
         self.assertEqual(len(response.json['token']), 16)
 
     def test_fortune_post(self):
-        pass
+        response = self.client.get('/token/')
+        result = self.set_fortune(response.json['token'], self.fortunes[0])
+
+        self.assert200(result)
+        self.assertEqual('a', result.json['fortune'])
 
     def test_fortune_get(self):
-        pass
+        response = self.client.get('/token/')
+        result = self.set_fortune(response.json['token'], self.fortunes[0])
+        fortune = self.client.get('/fortune/', data={'token': response.json['token']})
+        
+        self.assert200(fortune)
+        self.assertEqual('a', fortune.json['fortune'])
+
+    def test_fortune_get_random(self):
+        token = self.client.get('/token/').json
+        results = [self.set_fortune(token['token'], x) for x in self.fortunes]
+
+        fortune = self.client.get('/fortune/', data=token)
+
+        self.assert200(fortune)
+        self.assertTrue(fortune.json['fortune'] in self.fortunes)
