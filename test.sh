2b1f0413df23971215031fde0e5a32c7abf71f1c
kryptn <kryptn@gmail.com>
2017-01-05 23:45:23 -0700

    ended up removing envvars again

--- test.sh
+++ test.sh
@@ -2,8 +2,6 @@ tf='.venv_test'
 
 virtualenv $tf -p python
 $tf/bin/pip install -e fortunate
-export FORTUNATE_SETTINGS=test_settings.py
 $tf/bin/python fortunate/setup.py test
-unset FORTUNATE_SETTINGS
 
 rm -rf $tf
